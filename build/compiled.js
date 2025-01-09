19a00effe99d595222e7de7193eff247df00cb27
Kenei Menning <menningkenei@gmail.com>
2025-01-09 12:27:54 -0600

    fix bug last color to all

--- build/compiled.js
+++ build/compiled.js
@@ -23,20 +23,20 @@
 
   // lib/core/markdownProcessor.js
   function addTextToHeaders(markdown) {
-    let lines = markdown.split("\n");
-    lines = lines.map((line) => {
-      let headerPattern = /^(#{1,6})\s+(.+)$/;
-      let match = line.match(headerPattern);
+    let currentHeaderLevel = null;
+    let currentColor = null;
+    return markdown.split("\n").map((line) => {
+      let match = line.match(/^#{1,6}\s+(.+)$/);
       if (match) {
-        let header = match[1];
+        let headerLevel = match[1].length;
         let headerContent = match[2];
-        let headerLevel = header.length;
-        let { cycleColor } = headerColors[headerLevel] || {};
-        return `${header} ==${headerContent}<!-- {"cycleColor": "${cycleColor}"} -->==`;
+        if (headerLevel > currentHeaderLevel) {
+          currentColor = headerColors[headerLevel].cycleColor;
+        }
+        return `${line}<!-- {"cycleColor": "${currentColor}"} -->`;
       }
       return line;
-    });
-    return lines.join("\n");
+    }).join("\n");
   }
 
   // lib/plugin.js
